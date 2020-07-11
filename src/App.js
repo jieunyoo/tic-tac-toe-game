88d454db9e0fb15db60f99c62e09b74ca9459f19
Jieun <jieunyoo201@gmail.com>
2020-07-11 16:20:50 -0500

    add header, directions

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import Game from './components/Game';
 
 const App = () => 
-	<div class="game-style">
-	
-	<Game />
-</div>
+
+	<div class="container">
+		<h1> Welcome to the classic game of <strong> Tic-Tac-Toe </strong> </h1>
+		<h2> Rules: X goes first. Then 0. </h2>
+		<h2> Click the box where you want to enter your X or O. </h2>
+		<div class="game-style">
+			<Game />
+		</div>
+	</div>
 
 export default App;
